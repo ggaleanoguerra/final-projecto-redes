e20d620171cd0322d2517f5c8c0eadaf8b6cdbd1
ggaleanoguerra <81437406+ggaleanoguerra@users.noreply.github.com>
2022-02-10 14:41:17 -0500

    Update crud.js

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,6 +1,6 @@
-//Invocamos a la conexion de la DB
+
 const conexion = require('../database/db');
-//GUARDAR un REGISTRO
+
 exports.save = (req, res)=>{
     const user = req.body.user;
     const rol = req.body.rol;
@@ -8,12 +8,12 @@ exports.save = (req, res)=>{
         if(error){
             console.log(error);
         }else{
-            //console.log(results);   
+         
             res.redirect('/');         
         }
 });
 };
-//ACTUALIZAR un REGISTRO
+
 exports.update = (req, res)=>{
     const id = req.body.id;
     const user = req.body.user;
